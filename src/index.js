141f4bc4ce76dea7020c3030f4b8aab750121cff
juan <juanalbertomunozsalinas@gmail.com>
2022-08-28 11:54:45 -0500

    Start zipp backend, working well

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+
+// capturar body
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+//console.log(process.env.TOKEN_SECRET)
+// Conexión a Base de datos
+//const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ncdk5.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://zippuserbd:S8s23XNsq7PaiLjS@cluster0.y6gqako.mongodb.net/?retryWrites=true&w=majority";
+
+const option = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(uri, option)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
+
+
+/*
+const uri = "mongodb+srv://zippuserbd:S8s23XNsq7PaiLjS@cluster0.y6gqako.mongodb.net/?retryWrites=true&w=majority";
+const opcion = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(uri,opcion)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
+*/
+// import routes
+const authRoutes = require('./routes/auth')
+// route middlewares
+app.use('/api/user', authRoutes); 
+
+
+/*
+app.get('/', (req, res) => {
+    res.json({
+        estado: true,
+        mensaje: 'funciona!'
+    })
+});
+*/
+
+// iniciar server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`servidor andando en: ${PORT}`)
+})
