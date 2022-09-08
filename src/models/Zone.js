2c80614acf28b3fc85f93ed194e75d7e958573f1
juan <juanalbertomunozsalinas@gmail.com>
2022-09-08 13:59:52 -0500

    modelo bd crud zones

--- src/models/Zone.js
+++ src/models/Zone.js
@@ -4,7 +4,14 @@ const zoneSchema = new Schema(
     {
         nameZone: {type: String, required: true}, 
         addressZone: {type: String, required: true}, 
-        id_User: {type: String, required: true}
+        id_User: {type: String, required: true},
+        gps: {type: String, required: true}, //longitude, latitude
+        valor: {type: Number, required: true},
+        tipo: {type: String, required: true}, //car, motocicle, bicycle, scooter
+        dispo: {type: String, required: true}, //hour, day, week, month
+        estado: {type: Boolean, required: true}, //true, false
+        recaudo: {type: Number} 
+
     }, 
     {
         versionKey: false
