dea89b5d6a29faaac9c27ecb0361f3f87b3ee50d
Santiago Yate <santiyate7@gmail.com>
2022-10-09 23:47:16 -0500

    The dispo, type, and location fields were commented out

--- src/models/Zone.js
+++ src/models/Zone.js
@@ -6,10 +6,10 @@ const zoneSchema = new Schema(
         nameZone: {type: String, required: true}, 
         addressZone: {type: String, required: true}, 
         id_User: {type: String, required: true},
-        gps: {latitud:{type: Number, requerired: true}, longitud:{type: Number, requerired: true}},  //longitude, latitude
-        valor: {type: Number, required: true},//$
-        tipo: {type: String, required: true}, //car, motocicle, bicycle, scooter
-        dispo: {type: String, required: true}, //hour, day, week, month
+        //gps: {latitud:{type: Number, requerired: true}, longitud:{type: Number, requerired: true}},  //longitude, latitude
+        //valor: {type: Number, required: true},//$
+        //tipo: {type: String, required: true}, //car, motocicle, bicycle, scooter
+        //dispo: {type: String, required: true}, //hour, day, week, month
         estado: {type: Boolean} //true, false default
 
     }, 
