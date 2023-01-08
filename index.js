ef97292283fceca15236bd27c363355deaded277
juan <juanalbertomunozsalinas@gmail.com>
2023-01-08 14:14:29 -0500

    cors2

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express")
 const cors = require ("cors")
+const app = express()
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
-const app = express()
-
 
 
 const PORT = process.env.PORT || 3001
